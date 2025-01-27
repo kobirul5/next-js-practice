ae63ff7dfdeedbca501d29825574fcbcd309186b
kobirul5 <kobirul05j@gmail.com>
2025-01-27 10:16:21 +0600

    added components folder and nav and footer mmove in components and etc

--- src/app/about/address/page.js
+++ src/app/about/address/page.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const About = () => {
+const Address = () => {
     return (
         <div>
-            About address
+            Address
         </div>
     );
 };
 
-export default About;
+export default Address;
